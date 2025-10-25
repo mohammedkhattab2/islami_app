1a46fe98e997da7392ed14fa73dd3860a5185e9c
mohammedkhattab <khattabk205@gmail.com>
2025-10-25 21:55:19 +0300

    again

--- lib/ui/screens/home/tabs/ahadeth/ahadeth_tab.dart
+++ lib/ui/screens/home/tabs/ahadeth/ahadeth_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:islami_app/ui/utilits/app_assets.dart';
 
 class Hadeth {
   String titile;
@@ -25,13 +26,33 @@ class _AhadethTabState extends State<AhadethTab> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.blue);
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(AppAssets.ahadethBackground),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [Image.asset(AppAssets.islamiLogo)],
+        ),
+      ),
+    );
   }
 
   void readAhadethfile() async {
     String ahadethFile = await rootBundle.loadString(
       "assets/files/ahadeth.txt",
     );
-    print(ahadethFile);
+    List<String> ahadethList = ahadethFile.split("#\r\n");
+    for (int i = 0; i < ahadethList.length; i++) {
+      String hadeth = ahadethList[i];
+      List<String> hadethlines = hadeth.split("\n");
+      String title = hadethlines.removeAt(0);
+      ahadeth.add(Hadeth(title, hadethlines.join()));
+      print("${ahadeth[i].titile}");
+    }
   }
 }
