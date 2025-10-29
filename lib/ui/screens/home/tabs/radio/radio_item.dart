8bc27b37634c48373a93094e38f2ae1ea176c7e4
mohammedkhattab <khattabk205@gmail.com>
2025-10-29 17:33:45 +0300

    finish radio ui

--- lib/ui/screens/home/tabs/radio/radio_item.dart
+++ lib/ui/screens/home/tabs/radio/radio_item.dart
@@ -9,19 +9,47 @@ class RadioItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(20),
+      margin: EdgeInsets.all(10),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12),
         color: AppColors.primary,
-        image: DecorationImage(image: AssetImage(AppAssets.radioTabBackground),),
+        image: DecorationImage(
+          image: AssetImage(AppAssets.radioTabBackground),
+          fit: BoxFit.fitWidth,
+        ),
       ),
       child: Column(
         children: [
-          Text("Radio Ibrahim Al-Akdar", style: AppTextStyles.blackBold16,),
-          IconButton(onPressed: (){}, icon: Icon(Icons.play_arrow_rounded , size: 20 , color: Colors.black,))
+          Text("Radio Ibrahim Al-Akdar", style: AppTextStyles.blackBold16),
+          SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.play_arrow_rounded,
+                  size: 35,
+                  color: Colors.black,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.stop_rounded, size: 35, color: Colors.black),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.volume_down_rounded,
+                  size: 35,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
         ],
-      ) 
-      
-      
+      ),
     );
   }
 }
